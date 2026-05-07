1992c5a002c2788e67ebe2c9dd85d65825b0ec99
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-07 13:39:12 +0200

    SUBMIT: public pages
    team & home

---
+++ resources/views/public/home.blade.php
@@ -0,0 +1,6 @@
+<x-site-layout>
+    <div class="p-8">
+        <h1 class="text-3xl font-bold">Bienvenue au club</h1>
+        <p class="mt-4">Salut! C'est la page d'accueil de mon club de foot.</p>
+    </div>
+</x-site-layout>
---
+++ resources/views/public/team.blade.php
@@ -0,0 +1,6 @@
+<x-site-layout>
+    <div class="p-8">
+        <h1 class="text-3xl font-bold">L'équipe</h1>
+        <p class="mt-4">Voici la liste de nos joueurs (à venir).</p>
+    </div>
+</x-site-layout>
