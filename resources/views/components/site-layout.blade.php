f949e8f9ff30e9943bc2281a1dec97e1070c24c1
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-07 13:44:14 +0200

    SUBMIT: layout page

---
+++ resources/views/components/site-layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Club de Foot</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+<nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href='/'>Olympic Anderlecht</a>
+        <div>
+            <a href="{{ route('home') }}" class="text-gray-300 hover:text-white px-3">Accueil</a>
+            <a href="{{ route('team') }}" class="text-gray-300 hover:text-white px-3">L'équipe</a>
+        </div>
+    </div>
+
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
