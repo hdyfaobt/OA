be08fc4ce3567cb1502282d41219ed2e1799dd72
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-10 03:22:57 +0200

    SUBMIT: navlist

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -6,12 +6,17 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<nav class="bg-gray-800 p-4">
+<nav class="bg-red-600 text-white p-4">
     <div class="container mx-auto flex justify-between items-center">
-        <a href='/'>Olympic Anderlecht</a>
+<a href="/" class="text-white font-bold text-xl">Olympic Anderlecht</a>
         <div>
-            <a href="{{ route('home') }}" class="text-gray-300 hover:text-white px-3">Accueil</a>
-            <a href="{{ route('team') }}" class="text-gray-300 hover:text-white px-3">L'équipe</a>
+            <a href="{{ route('home') }}" class="text-white hover:text-gray-200 px-3">Accueil</a>
+            <a href="{{ route('team') }}" class="text-white hover:text-gray-200 px-3">Nos équipes</a>
+            <a href="{{ route('calendar') }}" class="text-white hover:text-gray-200 px-3">Matchs</a>
+            <a href="{{ route('results') }}" class="text-white hover:text-gray-200 px-3">Resultat</a>
+            <a href="{{ route('contact') }}" class="text-white hover:text-gray-200 px-3">Contact</a>
+            <a href="{{ route('about') }}" class="text-white hover:text-gray-200 px-3">A propos</a>
+            
         </div>
     </div>
 </nav>
