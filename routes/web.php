7500547b0b62abc53b7d8225a2fdb170dfb5d6c1
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-10 03:22:32 +0200

    SUBMIT: routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\Public\PublicController::class, 'home'])->name('home');
 Route::get('/team', [App\Http\Controllers\Public\PublicController::class, 'team'])->name('team');
+Route::get('/calendar', [App\Http\Controllers\Public\PublicController::class, 'calendar'])->name('calendar');
+Route::get('/contact', [App\Http\Controllers\Public\PublicController::class, 'contact'])->name('contact');
+Route::get('/results', [App\Http\Controllers\Public\PublicController::class, 'results'])->name('results');
+Route::get('/about', [App\Http\Controllers\Public\PublicController::class, 'about'])->name('about');
+Route::get('/admin/players', [App\Http\Controllers\Admin\PlayerController::class, 'index'])->name('admin.players.index');
+Route::get('/admin/players/{player}', [App\Http\Controllers\Admin\PlayerController::class, 'show'])->name('admin.players.show');
 
 Route::get('/dashboard', function () {
     return view('userzone.dashboard');
