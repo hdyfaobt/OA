fb6124e74a8bec750ba1b54f1d97bce03d9eb005
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-10 03:24:17 +0200

    SUBMIT: controller public

--- app/Http/Controllers/Public/PublicController.php
+++ app/Http/Controllers/Public/PublicController.php
@@ -12,9 +12,27 @@ public function home()
 {
     return view('public.home');
 }
-
 public function team()
 {
     return view('public.team');
 }
+public function calendar()
+{
+    return view('public.calendar');
 }
+public function contact()
+{
+    return view('public.contact');
+}
+public function results()
+{
+    return view('public.results');
+}   
+public function about()
+{
+    return view('public.about');
+}
+
+
+
+}   
