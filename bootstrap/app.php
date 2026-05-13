9d3ca842c630aed7d8ffe8ffc8bec2df23881b9c
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-13 12:06:26 +0200

    SUBMIT: middleware, alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->alias([
+    'is_admin' => \App\Http\Middleware\IsAdmin::class,
+]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
