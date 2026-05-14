7360834c9c26b709999e47625b3ae799e909d03c
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-14 18:07:12 +0200

    SUBMIT: delete sheddule/ results

--- resources/views/public/calendar.blade.php
+++
@@ -1,6 +0,0 @@
-<x-site-layout>
-    <div class="p-8">
-        <h1 class="text-3xl font-bold">Calendrier</h1>
-        <p class="mt-4">voici notre calendrier de matchs</p>
-    </div>
-</x-site-layout>
--- resources/views/public/results.blade.php
+++
@@ -1,6 +0,0 @@
-<x-site-layout>
-    <div class="p-8">
-        <h1 class="text-3xl font-bold">Resultats</h1>
-        <p class="mt-4">voici notre page de derniers resultats</p>
-    </div>
-</x-site-layout>
