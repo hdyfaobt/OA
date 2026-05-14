68046acc31a9a36a63c9c32122e8db15bdb0a932
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-05-14 18:07:32 +0200

    SUBMIT: info about

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -1,6 +1,35 @@
 <x-site-layout>
-    <div class="p-8">
-        <h1 class="text-3xl font-bold">A propos</h1>
-        <p class="mt-4">voici notre page a propos de nous</p>
+    
+    <div class="p-8 max-w-3xl">
+        <h1 class="text-5xl font-bold"> Olympic Anderlecht</h1>
+        <br><br>
+        <h1 class="text-3xl font-bold">Notre histoire</h1>
+        <br>
+        <p class="mt-4">
+            
+            Olympic Anderlecht, anciennement Bon Air Sport, est un club de football belge basé dans le quartier du Bon Air à Anderlecht . Affilié à la RBFA sous le numéro 8889, il arbore les couleurs rouge et bleu.
+            <br>
+            <br>
+            <h2 class="text-xl font-bold">Histoire</h2>
+            <br>
+            Le club a été actif dans le football amateur et corporatif pendant plusieurs années. Dans les années 1960, il a commencé à jouer sur un terrain situé sur Itterbeekse Laan. 
+            En 1983, sous la présidence de Jean Rousseau, il a décidé de rejoindre la Fédération belge de football, où il a reçu le numéro d'enregistrement 8889. 
+            Le Bon Air Sport a alors débuté en division provinciale de bas niveau.
+            <br>
+            <br>
+            Au cours de la décennie suivante, Bon Air Sport parvint à accéder à la troisième division provinciale.
+            En 1998, le club retomba brièvement en quatrième division provinciale, le plus bas niveau. 
+            Cependant, Bon Air remporta immédiatement son championnat la saison suivante et retrouva ainsi la troisième division provinciale en 1999. 
+            Après avoir remporté les séries éliminatoires, le club accéda directement à la deuxième division provinciale en 2000.
+            <br>
+            <br>
+            Bon Air Sport a évolué en deuxième division provinciale pendant plusieurs années. En 2007, le club a remporté le championnat et accédé à la première division provinciale. 
+            Relégué en 2009, il a réussi à remonter en première division provinciale en 2011 grâce aux barrages.
+            
+            En 2019, le club a changé de nom et est devenu Olympic Anderlecht. Son numéro d'immatriculation a été conservé.
+            la presidence du club a été confiée à Iacine Talhaoui, qui a succédé à Jean Rousseau, Mohamed Tabakkalt(paix a leurs ames).
+        </p>
     </div>
+    
+
 </x-site-layout>
